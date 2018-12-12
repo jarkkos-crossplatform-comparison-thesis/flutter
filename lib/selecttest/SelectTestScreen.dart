69f9c19dc59a081fda6dd85af7b27db8ddea81e1
Jarkko Saarinen <jsaarinen92@gmail.com>
2018-12-12 14:31:56 +0200

    Make SelectTestScreen buttons not full screen width

--- lib/selecttest/SelectTestScreen.dart
+++ lib/selecttest/SelectTestScreen.dart
@@ -40,7 +40,7 @@ class SelectTestScreen extends StatelessWidget {
         Column(
           children: <Widget>[
             Text("Flutter"),
-            Text("Version whatever"),
+            Text("Version 2"),
             Text("Select Test"),
           ],
         ),
@@ -56,81 +56,117 @@ class SelectTestScreen extends StatelessWidget {
   Widget _buildNavigationButtons(BuildContext context) {
     return Column(
       children: <Widget>[
-        SizedBox(
-            width: double.infinity,
-            child: RaisedButtonTapDown(
-              onPressed: () {
-                _gotoButtonDelayScreen(context);
-              },
-              child: Text("Button delay"),
-            )),
-        SizedBox(
-            width: double.infinity,
-            child: RaisedButtonTapDown(
-              onPressed: () {
-                _gotoLocalListItemsScreen(context);
-              },
-              child: Text("Local listview"),
-            )),
-        SizedBox(
-            width: double.infinity,
-            child: RaisedButtonTapDown(
-              onPressed: () {
-                _gotoNetworkListItemsScreen(context);
-              },
-              child: Text("Network listview"),
-            )),
-        SizedBox(
-            width: double.infinity,
-            child: RaisedButtonTapDown(
-              onPressed: () {
-                _gotoHeavyComputationScreen(context);
-              },
-              child: Text("Heavy computation"),
-            )),
-        SizedBox(
-            width: double.infinity,
-            child: RaisedButtonTapDown(
-              onPressed: () {
-                _gotoVibrationDelayScreen(context);
-              },
-              child: Text("Vibration delay"),
-            )),
-        SizedBox(
-            width: double.infinity,
-            child: RaisedButtonTapDown(
-              onPressed: () {
-                _gotoNotImplementedScreen(context);
-              },
-              child: Text("Third party notices"),
-            )),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: SizedBox(
+              width: 200.0,
+              child: RaisedButtonTapDown(
+                onPressed: () {
+                  _gotoButtonDelayScreen(context);
+                },
+                child: Text("Button delay"),
+              )),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: SizedBox(
+              width: 200.0,
+              child: RaisedButtonTapDown(
+                onPressed: () {
+                  _gotoLocalListItemsScreen(context);
+                },
+                child: Text("Local listview"),
+              )),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: SizedBox(
+              width: 200.0,
+              child: RaisedButtonTapDown(
+                onPressed: () {
+                  _gotoNetworkListItemsScreen(context);
+                },
+                child: Text("Network listview"),
+              )),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: SizedBox(
+              width: 200.0,
+              child: RaisedButtonTapDown(
+                onPressed: () {
+                  _gotoHeavyComputationScreen(context);
+                },
+                child: Text("Heavy computation"),
+              )),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: SizedBox(
+              width: 200.0,
+              child: RaisedButtonTapDown(
+                onPressed: () {
+                  _gotoVibrationDelayScreen(context);
+                },
+                child: Text("Vibration delay"),
+              )),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: SizedBox(
+              width: 200.0,
+              child: RaisedButtonTapDown(
+                onPressed: () {
+                  _gotoNotImplementedScreen(context);
+                },
+                child: Text("Third party notices"),
+              )),
+        ),
       ],
     );
   }
 
   void _gotoButtonDelayScreen(BuildContext context) {
     Navigator.push(
-        context, NoAnimationMaterialPageRoute(builder: (context) => ButtonLatencyScreen()));
+        context,
+        NoAnimationMaterialPageRoute(
+            builder: (context) => ButtonLatencyScreen()));
   }
 
   void _gotoHeavyComputationScreen(BuildContext context) {
-    Navigator.push(context, NoAnimationMaterialPageRoute(builder: (context) => HeavyComputationScreen()));
+    Navigator.push(
+        context,
+        NoAnimationMaterialPageRoute(
+            builder: (context) => HeavyComputationScreen()));
   }
 
   void _gotoVibrationDelayScreen(BuildContext context) {
     Navigator.push(
-        context, NoAnimationMaterialPageRoute(builder: (context) => VibrationLatencyScreen()));
+        context,
+        NoAnimationMaterialPageRoute(
+            builder: (context) => VibrationLatencyScreen()));
   }
 
   void _gotoLocalListItemsScreen(BuildContext context) {
-    Navigator.push(context, NoAnimationMaterialPageRoute(builder: (context) => ListItemsScreen(title: "Local listview", dataLoader: LocalDataLoader())));
+    Navigator.push(
+        context,
+        NoAnimationMaterialPageRoute(
+            builder: (context) => ListItemsScreen(
+                title: "Local listview", dataLoader: LocalDataLoader())));
   }
 
   void _gotoNetworkListItemsScreen(BuildContext context) {
-    Navigator.push(context, NoAnimationMaterialPageRoute(builder: (context) => ListItemsScreen(title: "Network listview", dataLoader: NetworkDataLoader())));
+    Navigator.push(
+        context,
+        NoAnimationMaterialPageRoute(
+            builder: (context) => ListItemsScreen(
+                title: "Network listview", dataLoader: NetworkDataLoader())));
   }
 
   void _gotoNotImplementedScreen(BuildContext context) {
-    Navigator.push(context, NoAnimationMaterialPageRoute(builder: (context) => NotImplementedScreen()));
+    Navigator.push(
+        context,
+        NoAnimationMaterialPageRoute(
+            builder: (context) => NotImplementedScreen()));
   }
 }
